83a88e548b0ab7e4b414bbf789f30ceb49995438
arvindkaur83 <72114508+arvindkaur83@users.noreply.github.com>
2026-02-22 05:41:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
-function scrollToSection(sectionId) {
-  document.getElementById(sectionId).scrollIntoView({
-    behavior: "smooth"
+// Load Navbar dynamically
+fetch("navbar.html")
+  .then(res => res.text())
+  .then(data => {
+    document.getElementById("navbar").innerHTML = data;
+
+    // Highlight active link
+    const links = document.querySelectorAll(".menu a");
+    links.forEach(link => {
+      if (link.href === window.location.href) {
+        link.classList.add("active");
+      }
+    });
   });
-}
