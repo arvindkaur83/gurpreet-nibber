8cfde4391d236e6760ea0b9d3027fac04ee12922
Arvind Kaur <arvindkaur.nibber@gmail.com>
2026-05-22 04:47:00 +0530

    Added Firebase App Check integration

--- firebase.js
+++ firebase.js
@@ -1,3 +1,11 @@
 firebase.initializeApp(firebaseConfig);
 
 const db = firebase.firestore();
+
+// 🔐 App Check
+const appCheck = firebase.appCheck();
+
+appCheck.activate(
+  "6LemS_YsAAAAAAqHAU0w5LPX8KwrV1llavFIq4q0",
+  true // auto refresh tokens
+);
