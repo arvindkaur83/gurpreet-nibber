135153f7034f62ebb30e903bea99c580105f5bf4
Arvind Kaur <arvindkaur.nibber@gmail.com>
2026-05-22 04:11:05 +0530

    Added config.js for Firebase live site

---
+++ config.js
@@ -0,0 +1,10 @@
+// config.js (LOCAL ONLY, DO NOT PUSH TO GITHUB)
+const firebaseConfig = {
+  apiKey: "AIzaSyDUlZQ_79NaRA8uQiLCQ0I5RlQCoo1LsMs",
+  authDomain: "gurpreet-website.firebaseapp.com",
+  projectId: "gurpreet-website",
+  storageBucket: "gurpreet-website.firebasestorage.app",
+  messagingSenderId: "61506601467",
+  appId: "1:61506601467:web:4dfeac9aa3a85599050b5e",
+  measurementId: "G-2HQP1QDK7X"
+};
